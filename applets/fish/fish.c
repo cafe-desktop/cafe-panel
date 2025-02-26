791302f9a881a8a2786f5839c7a0a6856a6eb3ed
Pablo Barciela <scow@riseup.net>
2025-02-26 09:40:10 +0100

    update copyright to 2025

--- applets/fish/fish.c
+++ applets/fish/fish.c
@@ -562,7 +562,7 @@ static void display_about_dialog(CtkAction* action, FishApplet* fish)
 		"copyright", _("Copyright \xc2\xa9 1998-2002 Free Software Foundation, Inc.\n"
 		               "Copyright \xc2\xa9 2002-2005 Vincent Untz\n"
 		               "Copyright \xc2\xa9 2012-2020 MATE developers\n"
-		               "Copyright \xc2\xa9 2022-2024 Pablo Barciela"),
+		               "Copyright \xc2\xa9 2022-2025 Pablo Barciela"),
 		"documenters", documenters,
 		"logo-icon-name", FISH_ICON,
 		"translator-credits", _("translator-credits"),
